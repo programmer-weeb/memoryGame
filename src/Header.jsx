4f98cddb88572ab4b26d659fee20a1c7d18faa5c
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-17 07:34:32 +0200

    feat: add Header component and implement interactive Card component for memory game

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold p-4 m-4 tracking-tight text-gray-800">
+        Memory Game
+      </h1>
+    </>
+  );
+}
+
+export default Header;
